7690d6d825859a98c6224433959bd802d1adde9e
mutsuya <mutsuya_masato@yahoo.co.jp>
2020-03-23 15:07:33 +0900

    AssemblyInfoに設定ファイルについて記載

--- Log4NetSample/My Project/AssemblyInfo.vb
+++ Log4NetSample/My Project/AssemblyInfo.vb
@@ -32,3 +32,4 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyVersion("1.0.0.0")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: log4net.Config.XmlConfigurator(Watch:=True)>
