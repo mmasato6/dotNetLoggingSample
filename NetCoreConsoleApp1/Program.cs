7ae27c9c19e429b669d8f5f027c019d49d9fd8bb
mutsuya <mutsuya_masato@yahoo.co.jp>
2020-07-17 17:06:24 +0900

    .net core console app から consoleにログ出力

---
+++ NetCoreConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace NetCoreConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loggerFactory = LoggerFactory.Create(builder => 
+            {
+                builder
+                .AddFilter($"{nameof(NetCoreConsoleApp1)}.{nameof(Program)}",LogLevel.Trace)
+                .AddConsole()
+                .AddDebug()
+                ;
+            });
+            ILogger logger = loggerFactory.CreateLogger<Program>();
+            logger.LogInformation("Information dayo");
+            logger.LogTrace("Trace dayo");
+            logger.LogDebug("Debug dayo");
+            logger.LogWarning("Warning dayo");
+            logger.LogError("Error dayo");
+            Console.WriteLine("Hello World!");
+            Console.ReadLine(); //そのまま終了するとコンソールにログが出ない。ログが出る前にプロセスが終わってる？(未確認)
+        }
+    }
+}
