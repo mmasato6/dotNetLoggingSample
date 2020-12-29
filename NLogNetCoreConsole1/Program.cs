8f16db08bfb18f290a82e846883bf52b88f02d5d
mutsuya <mutsuya_masato@yahoo.co.jp>
2020-12-29 15:50:35 +0900

    NLog 公式チュートリアル用 プロジェクト作成

---
+++ NLogNetCoreConsole1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NLogNetCoreConsole1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
