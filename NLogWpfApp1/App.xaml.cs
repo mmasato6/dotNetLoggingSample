bc55dcec099551e5abc9d8d60e68d3f06641c899
mutsuya <mutsuya_masato@yahoo.co.jp>
2021-02-11 15:12:26 +0900

    Application.Startup でログ出力を実装

--- NLogWpfApp1/App.xaml.cs
+++ NLogWpfApp1/App.xaml.cs
@@ -5,6 +5,10 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace NLogWpfApp1
 {
@@ -13,5 +17,34 @@ namespace NLogWpfApp1
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())//From NuGet Package Microsoft.Extensions.Configuration.Json
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+            var serviceProvider = BuildDi(config);
+            using (serviceProvider as IDisposable)
+            {
+                var runner = serviceProvider.GetRequiredService<Runner>();
+                runner.DoAction("Action1");
+                // throw new Exception("sample exception"); //例外の動作確認用
+            }
+        }
+
+        private IServiceProvider BuildDi(IConfiguration config)
+        {
+            return new ServiceCollection()
+                .AddTransient<Runner>()
+                .AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                    loggingBuilder.AddNLog(config);
+                }
+                )
+                .BuildServiceProvider();
+        }
+
     }
 }
